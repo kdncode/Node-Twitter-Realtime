890f951e5632bf115e13ce15df12c40642c0aff5
kdncode <kdncode@gmail.com>
2018-05-03 17:18:46 -0500

    finished signin + home/tweet page

--- routes/user.js
+++ routes/user.js
@@ -21,10 +21,10 @@ router.route('/signup')
                 user.photo = user.gravatar();
                 user.password = req.body.password;
                 user.save(function(err) {
-                    // req.logIn(user, function(err) {
+                    req.logIn(user, function(err) {
                         if (err) return next(err);
                         res.redirect('/');
-                    // })
+                    })
                 })
             }
         });
